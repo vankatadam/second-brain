f5a50a3beb87eb4f313795fbe79c465fef1050b9
Adam Vankat <vankatadam@gmail.com>
2025-04-23 16:09:16 +0200

    testing some stuff in next api

---
+++ react/testing-all-next/app/basicPagination/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function BasicPagination() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(6);
+
+  useEffect(() => {
+    fetch(`/api/todos?page=${currentPage}&limit=${itemsPerPage}`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((e) => console.log(e));
+  }, [currentPage, itemsPerPage]);
+
+  return (
+    <div className="flex justify-center">
+      <table className="w-full border table-fixed border-collapse">
+        <caption>A summary of the UK's most famous punk bands</caption>
+        <thead>
+          <tr>
+            <th scope="col" className="w-[25%]">
+              Title
+            </th>
+            <th scope="col" className="w-[50%]">
+              Description
+            </th>
+            <th scope="col" className="w-[50%]">
+              Done?
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((todo) => (
+            <tr key={todo.id}>
+              <th scope="row">{todo.title}</th>
+              <td>{todo.description}</td>
+              <td>{!!todo.done}</td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th scope="row">Total Todos</th>
+            <td>?</td>
+            <td className="flex gap-3">
+              <div>Page: </div>
+              {[...Array(10).keys()].map((e) => {
+                return (
+                  <button
+                    key={e}
+                    className={
+                      "text-black cursor-pointer " +
+                      (currentPage === e + 1 ? "font-extrabold underline" : "")
+                    }
+                    onClick={() => {
+                      setCurrentPage(e + 1);
+                    }}
+                  >
+                    {e + 1}
+                  </button>
+                );
+              })}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
---
+++ react/testing-all-next/tailwind.config.js
@@ -0,0 +1,9 @@
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+};
+export default config;
